a89c618de1fa09223526f5038c38cbc470c50299
qwomg <liorelgali@gmail.com>
2023-12-31 19:53:27 +0200

    Fixed the bugs! All of them! by thinking like a human!

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 let accentedBuffer, unaccentedBuffer;
+let scheduledSounds = [];
 
 // Load the audio files
 async function preloadSounds() {
@@ -12,7 +13,7 @@ async function loadAudioBuffer(url) {
     const arrayBuffer = await response.arrayBuffer();
     return audioContext.decodeAudioData(arrayBuffer);
 }
-let scheduledSounds = [];
+
 function playSound(buffer, time) {
     const source = audioContext.createBufferSource();
     source.buffer = buffer;
@@ -23,7 +24,7 @@ function playSound(buffer, time) {
     scheduledSounds.push({ source, time });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
     const beatsContainer = document.getElementById('beats-container');
     const tempoSlider = document.getElementById('tempo-slider');
@@ -34,48 +35,41 @@ document.addEventListener("DOMContentLoaded", function() {
     let currentBeatIndex = 0;
     let beats = [];
     let beatInterval;
-    
+
 
     let beatTimeout;
 
-function loadSettings() {
-    const storedBeats = localStorage.getItem('beats');
-    const storedTempo = localStorage.getItem('tempo');
-    if (storedBeats) {
-        beats = JSON.parse(storedBeats).map(beat => ({ ...beat })); // Create a new object for each beat
-    } else {
-        beats = new Array(4).fill(null).map(() => ({ type: 'normal', sound: 'default' }));
-    }
-    tempoSlider.value = storedTempo || 120;
-    tempoInput.value = tempoSlider.value;
-    numBeatsInput.value = beats.length;
-    renderBeats();
-    saveSettings();
-}
-
-    function loadBeats() {
+    function loadSettings() {
         const storedBeats = localStorage.getItem('beats');
-        beats = storedBeats ? JSON.parse(storedBeats) : new Array(parseInt(numBeatsInput.value)).fill({ type: 'normal', sound: 'unaccented' });
-        renderBeats();
-    }
-    function updateNumBeatsInput() {
-        const numBeatsInput = document.getElementById('num-beats');
+        const storedTempo = localStorage.getItem('tempo');
+        if (storedBeats) {
+            beats = JSON.parse(storedBeats).map(beat => ({ ...beat })); // Create a new object for each beat
+        } else {
+            beats = new Array(4).fill(null).map(() => ({ type: 'normal', sound: 'default' }));
+        }
+        tempoSlider.value = storedTempo || 120;
+        tempoInput.value = tempoSlider.value;
         numBeatsInput.value = beats.length;
-    }
-
-    document.getElementById('num-beats').addEventListener('change', (event) => {
-        const newNumBeats = parseInt(event.target.value);
-        adjustBeatsArray(newNumBeats);
         renderBeats();
         saveSettings();
-    });
+    }
     function adjustBeatsArray(newNumBeats) {
-        if (newNumBeats > beats.length) {
-            for (let i = beats.length; i < newNumBeats; i++) {
-                beats.push({ type: 'normal', sound: 'unaccented' }); // Each new beat is a distinct object
+        // Create a copy of current beats
+        const existingBeats = [...beats];
+
+        // Adjust length 
+        beats.length = newNumBeats;
+
+        // Copy over existing beats
+        existingBeats.forEach((beat, index) => {
+            if (index < newNumBeats) {
+                beats[index] = beat;
             }
-        } else if (newNumBeats < beats.length) {
-            beats.length = newNumBeats;
+        });
+
+        // Fill remaining with defaults
+        for (let i = existingBeats.length; i < newNumBeats; i++) {
+            beats[i] = { type: 'normal', sound: 'default' };
         }
     }
 
@@ -85,70 +79,61 @@ function loadSettings() {
     }
 
     function renderBeats() {
-        // Remove existing event listeners by replacing the container
         beatsContainer.innerHTML = '';
-        
+
         beats.forEach((beat, index) => {
             const beatElement = document.createElement('div');
             beatElement.classList.add('beat', 'p-2', 'rounded', 'text-white', 'text-center', 'm-1', 'cursor-pointer');
             beatElement.textContent = index + 1;
-            beatElement.style.width = '40px'; // Fixed width for beat blocks
-            beatElement.style.height = '40px'; // Fixed height for beat blocks
+            beatElement.style.width = '40px';
+            beatElement.style.height = '40px';
+
+            // Apply the existing style for each beat
             applyBeatStylesForElement(beat, beatElement);
-    
-            // Add event listener for each beat
+
             beatElement.addEventListener('click', () => {
                 toggleBeatType(index);
             });
-    
+
             beatsContainer.appendChild(beatElement);
         });
     }
+
     function applyBeatStylesForElement(beat, beatElement) {
         beatElement.className = 'beat p-2 rounded text-white text-center m-1 cursor-pointer';
         beatElement.classList.add(`beat-${beat.type}`);
     }
-    tempoSlider.addEventListener('input', function() {
+    tempoSlider.addEventListener('input', function () {
         tempoInput.value = tempoSlider.value;
         saveSettings();
     });
-    tempoInput.addEventListener('input', function() {
+    tempoInput.addEventListener('input', function () {
         tempoSlider.value = tempoInput.value;
         // Update the metronome tempo here if it's running
     });
     function toggleBeatType(index) {
-        const types = ['normal', 'accented', 'muted'];
-        if (index >= 0 && index < beats.length) { // Check if the index is valid
-            let currentTypeIndex = types.indexOf(beats[index].type);
-            beats[index].type = types[(currentTypeIndex + 1) % types.length];
-            applyBeatStylesForElement(beats[index], beatsContainer.children[index]);
-            saveSettings();
-        }
-    }
-    function applyBeatStyles() {
-        beats.forEach((beat, index) => {
-            const beatElement = beatsContainer.children[index];
-            beatElement.className = ''; // Clear all classes
-            beatElement.classList.add('beat', 'p-2', 'rounded', 'text-white', 'text-center', 'm-1', 'cursor-pointer');
 
-            if (beat.type === 'accented') {
-                beatElement.classList.add('beat-accented');
-            } else if (beat.type === 'muted') {
-                beatElement.classList.add('beat-muted');
-            } else {
-                beatElement.classList.add('beat-normal');
-            }
-            beatElement.classList.add(`beat-${beat.type}`);
-        });
+        // Toggle beat type
+        const types = ['normal', 'accented', 'muted'];
+        let currentTypeIndex = types.indexOf(beats[index].type);
+        beats[index].type = types[(currentTypeIndex + 1) % types.length];
+
+        // Re-apply styles to just the toggled beat
+        const beatEl = beatsContainer.children[index];
+        applyBeatStylesForElement(beats[index], beatEl);
+
+        // Save updated beats
+        saveSettings();
     }
+
     function highlightCurrentBeat(index) {
         clearBeatHighlighting();
         beatsContainer.children[index].classList.add('beat-playing');
     }
-    numBeatsInput.addEventListener('change', (event) => {
+
+    document.getElementById('num-beats').addEventListener('change', (event) => {
         const newNumBeats = parseInt(event.target.value);
-        beats = new Array(newNumBeats).fill({ type: 'normal', sound: 'unaccented' });
-        saveSettings();
+        adjustBeatsArray(newNumBeats);
         renderBeats();
         saveSettings();
     });
@@ -169,28 +154,28 @@ function loadSettings() {
         if (isPlaying) return;
         isPlaying = true;
         startStopBtn.textContent = 'Stop';
-    
+
         let nextBeatTime = audioContext.currentTime;
         scheduleBeats(nextBeatTime);
     }
 
     function scheduleBeats(time) {
         const beatDuration = 60 / tempoSlider.value;
-    
+
         while (time < audioContext.currentTime + 0.1) {
             playBeat(currentBeatIndex, time);
             currentBeatIndex = (currentBeatIndex + 1) % beats.length;
             time += beatDuration;
         }
-    
+
         beatTimeout = setTimeout(() => scheduleBeats(time), beatDuration / 2 * 1000);
     }
-    
+
     function stopMetronome() {
         if (!isPlaying) return;
         isPlaying = false;
         startStopBtn.textContent = 'Start';
-    
+
         // Stop and clear all scheduled sounds
         scheduledSounds.forEach(({ source, time }) => {
             if (time > audioContext.currentTime) {
@@ -198,7 +183,7 @@ function loadSettings() {
             }
         });
         scheduledSounds = []; // Reset the scheduled sounds
-    
+
         clearTimeout(beatTimeout);
         currentBeatIndex = 0;
         audioContext.suspend();
@@ -209,16 +194,27 @@ function loadSettings() {
         }
     }
     function playBeat(index, time) {
-        const beat = beats[index];
-        if (beat.type === 'accented') {
-            playSound(accentedBuffer, time);
-        } else if (beat.type === 'normal') {
-            playSound(unaccentedBuffer, time);
+        if (index >= 0 && index < beats.length) {
+            const beat = beats[index];
+            let buffer;
+
+            switch (beat.type) {
+                case 'accented':
+                    buffer = accentedBuffer;
+                    break;
+                case 'normal':
+                    buffer = unaccentedBuffer;
+                    break;
+                case 'muted':
+                    // Muted beats don't play sound
+                    return;
+            }
+
+            playSound(buffer, time);
+            highlightCurrentBeat(index);
         }
-        // Muted beats don't play sound
-        highlightCurrentBeat(index);
     }
-    
+
     preloadSounds();
     loadSettings();
     renderBeats();
